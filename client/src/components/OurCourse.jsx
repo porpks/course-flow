689123d6ef3115f841d7d105c3291a95057eb6c2
Dave <T_piyabud@outlook.com>
2023-09-01 02:41:22 +0700

    feat : comment app2.jsx & map data

--- client/src/components/OurCourse.jsx
+++ client/src/components/OurCourse.jsx
@@ -1,106 +1,116 @@
 import Navbar from "./Navbar.jsx";
-import CourseItem from "./CourseItem.jsx";
+import CourseItem from "./CourseItem";
 
+let courseData = [
+  {
+    course_id: 1,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "Service Design Essentials",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 2,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "Software Developer ",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 3,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "UX/UI Design Beginer",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 4,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "Product Design for Business",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 5,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "Service Design Essentials",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 6,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "Service Design Essentials",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 7,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "UX/UI Design Beginer",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 8,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "Service Design Essentials",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+  {
+    course_id: 9,
+    coursetype: "Course",
+    coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
+    coursename: "Software Developer",
+    coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
+    coursesummary: "6 Lesson",
+    totallearningtime: "6 Hours",
+  },
+];
 function OurCourse() {
-  let courseData = [
-    {
-      course_id: 1,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "Service Design Essentials",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 2,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "Software Developer ",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 3,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "UX/UI Design Beginer",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 4,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "Product Design for Business",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 5,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "Service Design Essentials",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 6,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "Service Design Essentials",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 7,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "UX/UI Design Beginer",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 8,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "Service Design Essentials",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-    {
-      course_id: 9,
-      coursetype: "Course",
-      coverimg: "http://dummyimage.com/350x350.png/dddddd/000000",
-      coursename: "Software Developer",
-      coursedetail: "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.",
-      coursesummary: "6 Lesson",
-      totallearningtime: "6 Hours",
-    },
-  ];
   return (
-    <section>
+    <div>
       <div className="topSection">
-        <h2>Our Courses</h2>
+        <h1>Our Courses</h1>
         <input
           className="search--ourCourse"
           type="search"
-          value=""
           placeholder="search..."
+          //   value=""
           // onChange={}
         />
       </div>
-      <div className="card--contrainer">
-        <CourseItem />
+      <div className="card--container">
+        {courseData.map((item) => (
+          <CourseItem
+            key={item.course_id}
+            coursetype={item.coursetype}
+            coverimg={item.coverimg}
+            coursename={item.coursename}
+            coursedetail={item.coursedetail}
+            coursesummary={item.coursesummary}
+            totallearningtime={item.totallearningtime}
+          />
+        ))}
       </div>
-    </section>
+    </div>
   );
 }
 export default OurCourse;
