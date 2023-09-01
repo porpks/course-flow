6b3a6f615efe813bcfc43d9487a90ae48f7a4818
Eart <thepray1313@gmail.com>
2023-09-01 22:41:59 +0700

    feat: add picture

--- client/src/components/UpdateProfile.jsx
+++ client/src/components/UpdateProfile.jsx
@@ -3,7 +3,9 @@ function UpdateProfile() {
     <div className="flex flex-col items-center bg-red-500 w-[1440px] h-[995px]">
       <h2 className="pt-[100px] pb-[72px]">Profile</h2>
       <div className="flex justify-between w-[930px] h-[521px]">
-        <div>pic</div>
+        <div>
+          <img src="../public/image/user_profile.png" />
+        </div>
         <div>input</div>
       </div>
     </div>
