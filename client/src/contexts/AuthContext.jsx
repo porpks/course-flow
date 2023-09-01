98f924e92d7d7ca278f3a4d7cb450f14f95032c4
PoR <por.pkp1997@gmail.com>
2023-09-02 01:47:51 +0700

    fix: register form reload after submit

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -4,25 +4,13 @@ import React, { useState } from "react";
 const AuthContext = React.createContext();
 
 function AuthProvider(props) {
-
   const [state, setState] = useState("eiei")
-  const [registerData, setRegisterData] = useState({})
-  const [loginData, setLoginData] = useState({})
-  const [isLoggedIn, setIsLoggedIn] = useState(true); // Set initial state to false
-
   return (
     <AuthContext.Provider
-      value={{
-        state, setState,
-        isLoggedIn, setIsLoggedIn,
-        registerData, setRegisterData,
-        loginData, setLoginData,
-      }}
-    >
+      value={{ state, setState }}>
       {props.children}
     </AuthContext.Provider>
-  );
-
+  )
 }
 const useAuth = () => React.useContext(AuthContext);
 
