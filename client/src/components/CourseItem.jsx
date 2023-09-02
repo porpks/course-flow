31da65c70c1303ba3845f13a8591be275617932c
Dave <t.piyabud@gmail.com>
2023-09-03 00:20:08 +0700

    feat : add style Course Card

--- client/src/components/CourseItem.jsx
+++ client/src/components/CourseItem.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import "./courseItem.css";
 
 function CourseItem(props) {
-  const {
-    coverimg,
-    coursetype,
-    coursename,
-    coursedetail,
-    coursesummary,
-    totallearningtime,
-    count,
-  } = props;
   return (
-    <div className="card--contrainer">
-      <div className="w">
-        <p></p>
-        <img src={coverimg} alt={coursename} />
+    <div className="Shadow1 w-[357px]  ">
+      <div className="topCard flex justify-center items-center ">
+        <img className="" src={props.coverimg} alt={props.coursename} />
       </div>
-      <div>
-        <p>{coursetype}</p>
-        <h2>{count}__{coursename}</h2>
-        <p>{coursedetail}</p>
-        <div className="footer--cord">
-          <p>{coursesummary}</p>
-          <p>{totallearningtime}</p>
+      <div className="bodyCard ">
+        <div className="border border-sky-500">
+          <p className="Body3 text-[--orange500]">{props.coursetype}</p>
+          <h2 className="H3 text-[#000] py-[8px]">{props.coursename}
+          </h2>
+          <p className="Body2 text-[--gray700]">{props.coursedetail}</p>
+        </div>
+      </div>
+      <div className="footerCard">
+        <div className="footer-Line"></div>
+        <div className="footerContents">
+          <div className="lesson-count-container">
+            <img src="../../public/lesson-count-icon.svg" alt="" />
+            <p className="courseSummary Body2 text-[--gray700] mx-[8px] ">
+              {props.coursesummary}
+            </p>
+          </div>
+          <div className="learningTime-container">
+            <img src="../../public/learningtime-icon.svg" alt="" />
+            <p className="Body2 text-[--gray700] mx-[8px]">
+              {props.totallearningtime}
+            </p>
+          </div>
         </div>
       </div>
     </div>
