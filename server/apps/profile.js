5e4c6a909f735003cf1dcc438e9c116aef381f70
Eart <thepray1313@gmail.com>
2023-09-05 00:34:29 +0700

    feat: create api for fetch and edit userProfile

--- server/apps/profile.js
+++ server/apps/profile.js
@@ -1,19 +1,62 @@
 import { Router } from "express";
+import supabase from "../utils/db.js";
 
 const profileRouter = Router();
 
 profileRouter.get("/:userId", async (req, res) => {
-    const userId = req.params.userId
+  try {
+    const userId = req.params.userId;
+
+    if (!userId || typeof userId !== "string") {
+      return res.status(400).json({
+        message: "Invalid url.",
+      });
+    }
+
+    const { data, error } = await supabase
+      .from("register")
+      .select("full_name, dateofbirth, edu_background, email")
+      .eq("user_id", userId);
+
+    if (error) {
+      return res.status(500).json({
+        message: "An error occurred while fetching data.",
+        error: error.message,
+      });
+    }
+
+    if (!data || data.length === 0) {
+      return res.status(404).json({
+        message: "User not found.",
+      });
+    }
+
     return res.json({
-        message: `user ${userId}`,
-    })
-})
+      message: "User profile data retrieved successfully.",
+      data: data[0],
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
 
 profileRouter.put("/:userId", async (req, res) => {
-    const userId = req.params.userId
-    return res.json({
-        message: `user ${userId}`,
-    })
-})
+  const userId = req.params.userId;
 
-export default profileRouter
+  const { data, error } = await supabase
+    .from("register")
+    .update({
+      full_name: req.body.full_name,
+      dateofbirth: req.body.dateofbirth,
+      edu_background: req.body.edu_background,
+      email: req.body.email,
+    })
+    .eq("user_id", userId);
+  return res.json({
+    message: "You profile has been update",
+  });
+});
+
+export default profileRouter;
