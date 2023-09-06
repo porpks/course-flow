062f8908c031ce38a86c60852aee4d0b4a22821b
piyabud <T_piyabud@outlook.com>
2023-09-06 14:28:30 +0700

    faet: add page CourseDetailPage

--- client/src/pages/CourseDetailPage.jsx
+++ client/src/pages/CourseDetailPage.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Navbar from '../components/Navbar.jsx'
-import Footer from '../components/Footer.jsx'
-import CourseDetail from "../components/CourseDetail.jsx"
+import React from "react";
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+import CourseDetail from "../components/CourseDetail.jsx";
 
 function CourseDetailPage() {
   return (
     <>
-            <Navbar />
-            <CourseDetail />
-            <Footer />
-        </>
-  )
+      <Navbar />
+      <CourseDetail />
+      <Footer />
+    </>
+  );
 }
 
-export default CourseDetailPage
+export default CourseDetailPage;
