3d074135e05e17932858bb3617fa2ee1fbf97fd4
PoR <por.pkp1997@gmail.com>
2023-09-08 18:57:24 +0700

    feat: learnig page 1st draft

---
+++ client/src/components/LearningVdo.jsx
@@ -0,0 +1,19 @@
+
+// eslint-disable-next-line react/prop-types
+function LearningVdo({ videoHead, videoKey }) {
+    if (videoKey) {
+        return (
+            <>
+                <div className="">
+                    <h1 className="H2">{videoHead}</h1>
+                </div>
+                <div className="w-full">
+                    <h1 className="H3">VDO: {videoKey}</h1>
+                </div>
+            </>
+        )
+    }
+
+}
+
+export default LearningVdo
