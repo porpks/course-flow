63bd6a545e757265c06c24409c257aef68944527
Billy <abl.chakrit@gmail.com>
2023-09-11 20:37:19 +0700

    fix bug

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Instructure from "../components/Instructure.jsx";
 import SubFooter from "../components/SubFooter.jsx";
 import Footer from "../components/Footer.jsx";
 // import { useAuth } from '../contexts/AuthContext.jsx'
+import AssignmentBox from "../components/AssignmentBox.jsx";
 
 function HomePage() {
   // const { state } = useAuth()
@@ -13,6 +14,7 @@ function HomePage() {
       <Instructure />
       <SubFooter />
       <Footer />
+      <AssignmentBox />
     </>
   );
 }
