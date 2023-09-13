b3f43385369445b014b324a24e8d66c249e6c710
Punt Boworn.T <boworn.tr@gmail.com>
2023-09-13 09:40:47 +0700

    Feat : Commit Before Rebase

---
+++ client/src/assets/myCourseAssets/Ellipse5.jsx
@@ -0,0 +1,15 @@
+function Ellipse5(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width || "53"}
+      height={props.height || "74"}
+      viewBox="0 0 53 74"
+      fill="none"
+    >
+      <circle cx="37" cy="37" r="37" fill="#C6DCFF" />
+    </svg>
+  );
+}
+
+export default Ellipse5;
---
+++ client/src/assets/myCourseAssets/Ellipse6.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Ellipse6() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="11"
+        height="11"
+        viewBox="0 0 11 11"
+        fill="none"
+      >
+        <circle cx="5.5" cy="5.5" r="4" stroke="#2F5FAC" stroke-width="3" />
+      </svg>
+    </div>
+  );
+}
+
+export default Ellipse6;
