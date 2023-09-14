8c023b7326c364af4ce557fd095ee8487c0242c8
piyabud <T_piyabud@outlook.com>
2023-09-14 16:29:25 +0700

    feat : create API for subscribe couse

--- server/apps/mycourse.js
+++ server/apps/mycourse.js
@@ -23,16 +23,52 @@ MyCourseRouter.get("/:userID", async (req, res) => {
 
 MyCourseRouter.post("/", async (req, res) => {
   try {
+    const { user_id, course_id } = req.body;
+    const dataOnDataBase = await supabase
+      .from("user_courses")
+      .select("*")
+      .eq("user_id", user_id)
+      .eq("course_id", course_id);
+
+    if (dataOnDataBase.data.length > 0) {
+      return res.json({ error: `course ${course_id} already exists.` });
+    }
     const subCourse = {
       user_id: req.body.user_id,
       course_id: req.body.course_id,
       course_status: false,
     };
+    const { error } = await supabase.from("user_courses").insert(subCourse);
 
-    const { data, error } = await supabase.from().select().eq().eq();
+    if (error) {
+      throw error;
+    }
+
+    res.json({ message: `course ${course_id} has been added.` });
   } catch (error) {
     console.log(`Message error :${error}`);
+    res.status(500).json({ error: "An error occurred." });
   }
 });
+
+MyCourseRouter.get("/", async (req, res) => {
+  try {
+    const user_id = req.query.user_id;
+    const course_id = req.query.course_id;
+
+    const data = await supabase
+      .from("user_courses")
+      .select("*")
+      .eq("user_id", user_id)
+      .eq("course_id", course_id);
+
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching user course data:", error.message);
+    res.status(500).json({ error });
+  }
+});
+
+
 
 export default MyCourseRouter;
