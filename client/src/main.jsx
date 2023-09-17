49f9826f63b03533af8c91008c7cddeebd587574
Billy <abl.chakrit@gmail.com>
2023-09-17 17:53:04 +0700

    feat: Assignmentadmin page done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext.jsx";
-import jwtInterceptor from "./utils/jwtInterceptors.jsx";
-
-jwtInterceptor();
+// import { BrowserRouter } from "react-router-dom";
+// import { AuthProvider } from "./contexts/AuthContext.jsx";
+// import jwtInterceptor from "./utils/jwtInterceptors.jsx";
+import AssignmentAdminnListPage from "./pages/AssignmentAdminnListPage";
+// jwtInterceptor();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    {/* <BrowserRouter>
+      <AuthProvider> */}
+    {/* <App /> */}
+    <AssignmentAdminnListPage />
+    {/* </AuthProvider>
+    </BrowserRouter> */}
   </React.StrictMode>
 );
