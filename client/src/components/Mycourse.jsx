222cc87489e4ce5085d26a86699aabed4f0c481b
Punt Boworn.T <boworn.tr@gmail.com>
2023-09-18 17:39:20 +0700

    Feat : Commit 1

--- client/src/components/Mycourse.jsx
+++ client/src/components/Mycourse.jsx
@@ -1,4 +1,8 @@
 import Avatar from "@mui/material/Avatar";
+// import MenuItem from "@mui/material/MenuItem";
+// import MenuList from "@mui/material/MenuList";
+// import Stack from "@mui/material/Stack";
+// import Paper from "@mui/material/Paper";
 import MenuItem from "@mui/material/MenuItem";
 import MenuList from "@mui/material/MenuList";
 import Stack from "@mui/material/Stack";
@@ -11,6 +15,7 @@ import { useAuth } from "../contexts/AuthContext.jsx";
 import Ellipse5 from "../assets/myCourseAssets/Ellipse5";
 import Polygon3 from "../assets/myCourseAssets/Polygon3";
 import Cross5 from "../assets/myCourseAssets/Cross5";
+import { Button } from "@mui/base/Button";
 
 function MyCourse() {
   const [dataCourse, setDataCourse] = useState([]);
@@ -155,7 +160,7 @@ function MyCourse() {
 
   function AllCourse() {
     return (
-      <div className='grid grid-cols-2 gap-x-[26px] gap-y-[40px] w-[740px]'>
+      <div className="grid grid-cols-2 gap-x-[26px] gap-y-[40px] w-[740px]">
         {dataCourse.map((item) => (
           <div
             key={item.courses.course_id}
@@ -164,13 +169,16 @@ function MyCourse() {
               localStorage.removeItem("course_id");
               localStorage.setItem("course_id", item.courses.course_id);
               getDataCourse2();
-            }}>
+            }}
+          >
             <Link
+              className="no-underline"
               key={item.courses.course_id}
               to={`/learning/${localStorage.getItem("course_id")}`} // Define the route you want to navigate to
             >
               <CourseCard
                 key={item.courses.course_id}
+                courseid={item.courses.course_id}
                 coverimg={item.courses.cover_img}
                 coursename={item.courses.course_name}
                 coursedetail={item.courses.course_detail}
@@ -190,9 +198,10 @@ function MyCourse() {
         (item) => !item.course_status
       );
       return (
-        <div className='grid grid-cols-2 gap-x-[26px] gap-y-[40px] w-[740px]'>
+        <div className="grid grid-cols-2 gap-x-[26px] gap-y-[40px] w-[740px]">
           {inProgressCourses.map((item) => (
             <Link
+              className="no-underline"
               key={item.courses.course_id}
               to={`/learning/${item.courses.course_id}`}
               onClick={() => {
@@ -204,6 +213,7 @@ function MyCourse() {
             >
               <CourseCard
                 key={item.courses.course_id}
+                courseid={item.courses.course_id}
                 coverimg={item.courses.cover_img}
                 coursename={item.courses.course_name}
                 coursedetail={item.courses.course_detail}
@@ -220,9 +230,10 @@ function MyCourse() {
     if (dataCourse.length > 0) {
       const completeCourses = dataCourse.filter((item) => item.course_status);
       return (
-        <div className='grid grid-cols-2 gap-x-[26px] gap-y-[40px]  w-[740px]'>
+        <div className="grid grid-cols-2 gap-x-[26px] gap-y-[40px]  w-[740px]">
           {completeCourses.map((item) => (
             <Link
+              className="no-underline"
               key={item.courses.course_id}
               onClick={() => {
                 setCheckOnClick((q) => !q);
@@ -234,6 +245,7 @@ function MyCourse() {
             >
               <CourseCard
                 key={item.courses.course_id}
+                courseid={item.courses.course_id}
                 coverimg={item.courses.cover_img}
                 coursename={item.courses.course_name}
                 coursedetail={item.courses.course_detail}
@@ -248,46 +260,50 @@ function MyCourse() {
   }
 
   return (
-    <div className='w-[100%] flex flex-col justify-center items-center pt-[100px] mb-[200px] relative '>
-      <div className='w-[100%] flex flex-col justify-center items-center pt-[100px] mb-[200px] relative '>
-        <div className=' absolute right-0 top-[216px]'>
-          <Ellipse5 className='top-1/2 absolute' style={{ top: "50%" }} />
+    <div className="w-[100%] flex flex-col justify-center items-center pt-[100px] mb-[200px] relative ">
+      <div className="w-[100%] flex flex-col justify-center items-center pt-[100px] mb-[200px] relative ">
+        <div className=" absolute right-0 top-[216px]">
+          <Ellipse5 className="top-1/2 absolute" style={{ top: "50%" }} />
         </div>
-        <div className=' absolute right-[126.22px] top-[126px]'>
+        <div className=" absolute right-[126.22px] top-[126px]">
           <Polygon3 />
         </div>
-        <div className=' absolute left-[280px] top-[232px]'>
+        <div className=" absolute left-[280px] top-[232px]">
           <Cross5 />
         </div>
-        <div className=' absolute left-[43px] top-[159px]'>
+        <div className=" absolute left-[43px] top-[159px]">
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='27'
-            height='27'
-            viewBox='0 0 27 27'
-            fill='none'>
-            <circle cx='13.1741' cy='13.1741' r='13.1741' fill='#C6DCFF' />
+            xmlns="http://www.w3.org/2000/svg"
+            width="27"
+            height="27"
+            viewBox="0 0 27 27"
+            fill="none"
+          >
+            <circle cx="13.1741" cy="13.1741" r="13.1741" fill="#C6DCFF" />
           </svg>
         </div>
-        <div className=' absolute left-[102px] top-[100px]'>
+        <div className=" absolute left-[102px] top-[100px]">
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='11'
-            height='11'
-            viewBox='0 0 11 11'
-            fill='none'>
-            <circle cx='5.5' cy='5.5' r='4' stroke='#2F5FAC' strokeWidth='3' />
+            xmlns="http://www.w3.org/2000/svg"
+            width="11"
+            height="11"
+            viewBox="0 0 11 11"
+            fill="none"
+          >
+            <circle cx="5.5" cy="5.5" r="4" stroke="#2F5FAC" strokeWidth="3" />
           </svg>
         </div>
-        <div className='flex flex-col items-center justify-center '>
-          <h2 className='H2'>My Course</h2>
-          <div className='flex flex-row mt-[60px]'>
-            <Stack direction='row' spacing={2} className=''>
+        <div className="flex flex-col items-center justify-center ">
+          <h2 className="H2">My Course</h2>
+          {/* <div className="flex flex-row mt-[60px]">
+            <Stack direction="row" spacing={2} className="">
               <Paper>
-                <MenuList className='flex fle-row'>
+                <MenuList className="flex fle-row ">
                   <MenuItem
-                    style={{ backgroundColor: "red" }}
-                    onClick={handleAllCourse}>
+                    className="cursor-pointer "
+                    style={{}}
+                    onClick={handleAllCourse}
+                  >
                     All Course
                   </MenuItem>
                   <MenuItem onClick={handleInprogress}>Inprogress</MenuItem>
@@ -295,22 +311,42 @@ function MyCourse() {
                 </MenuList>
               </Paper>
             </Stack>
+          </div> */}
+          <div className="justify-start items-start gap-4 inline-flex mt-[60px]">
+            <div
+              onClick={() => handleAllCourse("All")}
+              className={`box-content cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
+            >
+              <div className="Body2">All Course</div>
+            </div>
+            <div
+              onClick={() => handleInprogress("All")}
+              className={`box-content cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
+            >
+              <div className="Body2">Inprogress</div>
+            </div>
+            <div
+              onClick={() => handleComplete("All")}
+              className={`box-content  cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
+            >
+              <div className="Body2">Complete</div>
+            </div>
           </div>
         </div>
-        <div className='flex flex-row mt-[40px]  '>
-          <div className='flex flex-col w-[357px] h-fit Shadow2 px-[24px] py-[32px] content-center items-center mr-[24px] rounded-lg  sticky top-0 '>
-            <div className=''>
-              <Avatar alt='' src={avatar} sx={{ width: 120, height: 120 }} />
-              <h2 className='my-[24px]'>{userName}</h2>
+        <div className="flex flex-row mt-[80px] ">
+          <div className="flex flex-col w-[357px] h-fit Shadow2 px-[24px] py-[32px] content-center items-center mr-[24px] rounded-lg  sticky top-0 ">
+            <div className="">
+              <Avatar alt="" src={avatar} sx={{ width: 120, height: 120 }} />
+              <h2 className="my-[24px]">{userName}</h2>
             </div>
-            <div className='flex flex-row '>
-              <div className='flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]'>
-                <p className='Body2'>Course Inprogress</p>
-                <p className='H3'>{inProgressCount}</p>
+            <div className="flex flex-row ">
+              <div className="flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]">
+                <p className="Body2">Course Inprogress</p>
+                <p className="H3">{inProgressCount}</p>
               </div>
-              <div className='flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]'>
-                <p className='Body2'>Course Complete</p>
-                <p className='H3'>{completeCount}</p>
+              <div className="flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]">
+                <p className="Body2">Course Complete</p>
+                <p className="H3">{completeCount}</p>
               </div>
             </div>
           </div>
