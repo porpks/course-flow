9201c8234be3d4f44af85698da1c3ae59daa0eca
Eart <thepray1313@gmail.com>
2023-09-19 23:26:10 +0700

    feat:add sidebar and course list admin page

--- client/src/pages/AdminPage/CourseListPage.jsx
+++ client/src/pages/AdminPage/CourseListPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Sidebar from "../../components/Sidebar";
+import Courselist from "../../components/adminCourseListComponent/courselist";
 
 function CourseListPage() {
-  return <div>CourseListPage</div>;
+  return (
+    <div className="flex">
+      <Sidebar isCoursePage={true} />
+      <Courselist />
+    </div>
+  );
 }
 
 export default CourseListPage;
