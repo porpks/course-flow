4bb3a67046f26f459f9521742b8494f90367583d
Punt Boworn.T <boworn.tr@gmail.com>
2023-09-24 16:24:22 +0700

    Resolve Conflict MyCourse Page

--- client/src/components/Mycourse.jsx
+++ client/src/components/Mycourse.jsx
@@ -18,25 +18,7 @@ import CircularIndeterminate from '../assets/loadingProgress'
 
 function MyCourse() {
   const [dataCourse, setDataCourse] = useState([])
-  const [dataCourse, setDataCourse] = useState([])
   // const [dataCourse, setDataCourse] = localStorage.getItem("dataCourse");
-  const [courseID, setCourseID] = useState(null)
-  const [allCourse, setAllCourse] = useState(true)
-  const [inprogress, setInprogress] = useState(false)
-  const [complete, setComplete] = useState(false)
-  const userName = localStorage.getItem('username')
-  const avatar = localStorage.getItem('userimage')
-  const [currentPage, setCurrentPage] = useState(1)
-  const itemsPerPage = 4
-  const [inProgressCount, setInProgressCount] = useState(0)
-  const [completeCount, setCompleteCount] = useState(0)
-  const [isLoading, setIsLoading] = useState(true)
-  const indexOfLastItem = currentPage * itemsPerPage
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage
-  const inProgressCourses = dataCourse.filter((item) => !item.course_status)
-  const completeCourses = dataCourse.filter((item) => item.course_status)
-  const currentData = dataCourse.slice(indexOfFirstItem, indexOfLastItem)
-  const totalPagesAllCourse = Math.ceil(dataCourse.length / itemsPerPage)
   const [courseID, setCourseID] = useState(null)
   const [allCourse, setAllCourse] = useState(true)
   const [inprogress, setInprogress] = useState(false)
@@ -58,15 +40,10 @@ function MyCourse() {
     inProgressCourses.length / itemsPerPage
   )
   const totalPagesComplete = Math.ceil(completeCourses.length / itemsPerPage)
-  )
-  const totalPagesComplete = Math.ceil(completeCourses.length / itemsPerPage)
   const handlePageChange = (event, newPage) => {
     setCurrentPage(newPage)
   }
-    setCurrentPage(newPage)
-  }
 
-  const [checkOnClick, setCheckOnClick] = useState(false)
   const [checkOnClick, setCheckOnClick] = useState(false)
   const {
     setIsShowVdo,
@@ -77,7 +54,6 @@ function MyCourse() {
     userId,
     setvideoUrl,
     userIdFromCookie,
-  } = useAuth()
   } = useAuth()
 
   useEffect(() => {
@@ -103,18 +79,6 @@ function MyCourse() {
     localStorage.removeItem('videoHead')
     getDataCourse(), setCurrentPage(1)
   }, [userId, allCourse, inprogress, complete])
-    localStorage.removeItem('sublessonName')
-    localStorage.removeItem('sublessonID')
-    localStorage.removeItem('isShowVdo')
-    localStorage.removeItem('isShowAsm')
-    localStorage.removeItem('pauseTime')
-    localStorage.removeItem('videoUrl')
-    localStorage.removeItem('pauseTime')
-    localStorage.removeItem('nonepause')
-    localStorage.removeItem('videoKey')
-    localStorage.removeItem('videoHead')
-    getDataCourse(), setCurrentPage(1)
-  }, [userId, allCourse, inprogress, complete])
 
   const getDataCourse = async () => {
     try {
@@ -122,31 +86,22 @@ function MyCourse() {
       const result = await axios.get(
         `http://localhost:4000/mycourse/${userIdFromCookie}`
       )
-      )
 
-      const newDataCourse = result.data.data
       const newDataCourse = result.data.data
 
       if (newDataCourse.length > 0) {
-        setDataCourse(newDataCourse)
         setDataCourse(newDataCourse)
       }
       const counts = newDataCourse.reduce(
         (accumulator, course) => {
           if (course.course_status === false || course.course_status === null) {
             accumulator.inProgressCount++
-            accumulator.inProgressCount++
           } else if (course.course_status === true) {
-            accumulator.completeCount++
             accumulator.completeCount++
           }
           return accumulator
-          return accumulator
         },
         { inProgressCount: 0, completeCount: 0 }
-      )
-      setInProgressCount(counts.inProgressCount)
-      setCompleteCount(counts.completeCount)
       )
       setInProgressCount(counts.inProgressCount)
       setCompleteCount(counts.completeCount)
@@ -155,24 +110,10 @@ function MyCourse() {
       setIsLoading(false)
     } catch (error) {
       console.error('Error fetching data:', error)
-      console.error('Error fetching data:', error)
     }
-  }
   }
 
   function handleCourseFilter(filter) {
-    if (filter === 'all') {
-      setAllCourse(true)
-      setInprogress(false)
-      setComplete(false)
-    } else if (filter === 'inProgress') {
-      setAllCourse(false)
-      setInprogress(true)
-      setComplete(false)
-    } else if (filter === 'complete') {
-      setAllCourse(false)
-      setInprogress(false)
-      setComplete(true)
     if (filter === 'all') {
       setAllCourse(true)
       setInprogress(false)
@@ -199,48 +140,18 @@ function MyCourse() {
       localStorage.removeItem('pauseTime')
       localStorage.removeItem('nonepause')
       localStorage.removeItem('videoKey')
-      localStorage.removeItem('sublessonName')
-      localStorage.removeItem('sublessonID')
-      localStorage.removeItem('isShowVdo')
-      localStorage.removeItem('isShowAsm')
-      localStorage.removeItem('pauseTime')
-      localStorage.removeItem('videoUrl')
-      localStorage.removeItem('pauseTime')
-      localStorage.removeItem('nonepause')
-      localStorage.removeItem('videoKey')
 
-      const result = await axios.get('http://localhost:4000/learn/videotime', {
       const result = await axios.get('http://localhost:4000/learn/videotime', {
         params: {
           userID: userId,
           courseID: localStorage.getItem('course_id'),
-          courseID: localStorage.getItem('course_id'),
         },
       })
-
-      console.log(result)
-      const data = result.data.data
-      console.log(data, 'data')
-
-      if (data !== undefined) {
+      const data = await result.data.data
+      console.log(data)
+      console.log(Object.keys(data).length > 0)
+      if (Object.keys(data).length > 0) {
         const handleShowVideo = (sublessonName, sublessonID) => {
-          setVideoHead(sublessonName)
-          localStorage.setItem('sublessonName', sublessonName)
-          setVideoKey(sublessonID)
-          localStorage.setItem('sublessonID', sublessonID)
-          localStorage.setItem('videoKey', sublessonID)
-          setIsShowVdo(true)
-          localStorage.setItem('isShowVdo', true)
-          setIsShowAsm(false)
-          localStorage.setItem('isShowAsm', false)
-          setPauseTime(data.sublesson_video_timestop)
-          localStorage.setItem(data.sublesson_video_timestop)
-          setvideoUrl(data.sublesson_video)
-          localStorage.setItem('videoUrl', data.sublesson_video)
-          localStorage.setItem('nonepause', false)
-          console.log(data.sublesson_video_timestop)
-        }
-        handleShowVideo(data.sublesson_name, data.sublesson_id)
           setVideoHead(sublessonName)
           localStorage.setItem('sublessonName', sublessonName)
           setVideoKey(sublessonID)
@@ -266,25 +177,13 @@ function MyCourse() {
         setPauseTime(0)
         localStorage.setItem('pauseTime', 0)
         localStorage.setItem('nonepause', true)
-        setIsShowVdo(true)
-        localStorage.setItem('isShowVdo', true)
-        setIsShowAsm(false)
-        localStorage.setItem('isShowAsm', false)
-        setPauseTime(0)
-        localStorage.setItem('pauseTime', 0)
-        localStorage.setItem('nonepause', true)
       }
     } catch (error) {
       console.log('there is no sublesson in this code')
-      console.log('there is no sublesson in this code')
     }
-  }
   }
 
   const CourseList = ({ dataCourse, status }) => {
-    const [coursesPerPage] = useState(4)
-    const indexOfLastItem = currentPage * coursesPerPage
-    const indexOfFirstItem = indexOfLastItem - coursesPerPage
     const [coursesPerPage] = useState(4)
     const indexOfLastItem = currentPage * coursesPerPage
     const indexOfFirstItem = indexOfLastItem - coursesPerPage
@@ -296,15 +195,7 @@ function MyCourse() {
         return !item.course_status
       } else if (status === 'complete') {
         return item.course_status
-      if (status === 'all') {
-        return true
-      } else if (status === 'inProgress') {
-        return !item.course_status
-      } else if (status === 'complete') {
-        return item.course_status
       }
-      return false
-    })
       return false
     })
 
@@ -312,16 +203,8 @@ function MyCourse() {
       indexOfFirstItem,
       indexOfLastItem
     )
-    )
 
     const handleClick = (courseId) => {
-      setCheckOnClick((q) => !q)
-      setCourseID(courseId)
-      localStorage.removeItem('course_id')
-      localStorage.setItem('course_id', courseId)
-      getDataCourse2()
-      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-    }
       setCheckOnClick((q) => !q)
       setCourseID(courseId)
       localStorage.removeItem('course_id')
@@ -348,110 +231,11 @@ function MyCourse() {
         />
       </Link>
     ))
-    ))
     return (
       <div className="grid grid-cols-2 gap-x-[26px] gap-y-[40px] w-[740px]">
-        {dataCourse.map((item) => (
-          <div
-            key={item.courses.course_id}
-            onClick={() => {
-              setCheckOnClick((q) => !q)
-              setCourseID(item.courses.course_id)
-              localStorage.removeItem('course_id')
-              localStorage.setItem('course_id', item.courses.course_id)
-              getDataCourse2()
-              window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-            }}
-          >
-            <Link
-              className="no-underline"
-              key={item.courses.course_id}
-              to={`/learning/${item.courses.course_id}`} // Define the route you want to navigate to
-            >
-              <CourseCard
-                key={item.courses.course_id}
-                courseid={item.courses.course_id}
-                coverimg={item.courses.cover_img}
-                coursename={item.courses.course_name}
-                coursedetail={item.courses.course_detail}
-                coursesummary={item.courses.course_summary}
-                totallearningtime={item.courses.total_time}
-              />
-            </Link>
-          </div>
-        ))}
+        {courseCards}
       </div>
     )
-    )
-  }
-
-  function Inprogress() {
-    if (dataCourse.length > 0) {
-      const inProgressCourses = dataCourse.filter((item) => !item.course_status)
-      return (
-        <div className="grid grid-cols-2 gap-x-[26px] gap-y-[40px] w-[740px]">
-          {inProgressCourses.map((item) => (
-            <Link
-              className="no-underline"
-              key={item.courses.course_id}
-              to={`/learning/${item.courses.course_id}`}
-              onClick={() => {
-                setCheckOnClick((q) => !q)
-                setCourseID(item.courses.course_id)
-                localStorage.removeItem('course_id')
-                localStorage.setItem('course_id', item.courses.course_id)
-                getDataCourse2()
-                window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-              }} // Define the route you want to navigate to
-            >
-              <CourseCard
-                key={item.courses.course_id}
-                courseid={item.courses.course_id}
-                coverimg={item.courses.cover_img}
-                coursename={item.courses.course_name}
-                coursedetail={item.courses.course_detail}
-                coursesummary={item.courses.course_summary}
-                totallearningtime={item.courses.total_time}
-              />
-            </Link>
-          ))}
-        </div>
-      )
-    }
-  }
-  function Complete() {
-    if (dataCourse.length > 0) {
-      const completeCourses = dataCourse.filter((item) => item.course_status)
-      return (
-        <div className="grid grid-cols-2 gap-x-[26px] gap-y-[40px]  w-[740px]">
-          {completeCourses.map((item) => (
-            <Link
-              className="no-underline"
-              key={item.courses.course_id}
-              onClick={() => {
-                setCheckOnClick((q) => !q)
-                setCourseID(item.courses.course_id)
-                localStorage.removeItem('course_id')
-                localStorage.setItem('course_id', item.courses.course_id)
-                getDataCourse2()
-                window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-              }}
-              to={`/learning/${item.courses.course_id}`} // Define the route you want to navigate to
-            >
-              <CourseCard
-                key={item.courses.course_id}
-                courseid={item.courses.course_id}
-                coverimg={item.courses.cover_img}
-                coursename={item.courses.course_name}
-                coursedetail={item.courses.course_detail}
-                coursesummary={item.courses.course_summary}
-                totallearningtime={item.courses.total_time}
-              />
-            </Link>
-          ))}
-        </div>
-      )
-    }
   }
   if (isLoading) {
     return (
@@ -466,28 +250,15 @@ function MyCourse() {
     <div className="w-[100%] flex flex-col justify-center items-center pt-[100px] mb-[200px] relative ">
       <div className=" absolute right-0 top-[216px]">
         <Ellipse5 className="top-1/2 absolute" style={{ top: '50%' }} />
-    <div className="w-[100%] flex flex-col justify-center items-center pt-[100px] mb-[200px] relative ">
-      <div className=" absolute right-0 top-[216px]">
-        <Ellipse5 className="top-1/2 absolute" style={{ top: '50%' }} />
       </div>
-      <div className=" absolute right-[126.22px] top-[126px]">
       <div className=" absolute right-[126.22px] top-[126px]">
         <Polygon3 />
       </div>
       <div className=" absolute left-[280px] top-[232px]">
-      <div className=" absolute left-[280px] top-[232px]">
         <Cross5 />
       </div>
       <div className=" absolute left-[43px] top-[159px]">
-      <div className=" absolute left-[43px] top-[159px]">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="27"
-          height="27"
-          viewBox="0 0 27 27"
-          fill="none"
-        >
-          <circle cx="13.1741" cy="13.1741" r="13.1741" fill="#C6DCFF" />
           xmlns="http://www.w3.org/2000/svg"
           width="27"
           height="27"
@@ -498,15 +269,7 @@ function MyCourse() {
         </svg>
       </div>
       <div className=" absolute left-[102px] top-[100px]">
-      <div className=" absolute left-[102px] top-[100px]">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="11"
-          height="11"
-          viewBox="0 0 11 11"
-          fill="none"
-        >
-          <circle cx="5.5" cy="5.5" r="4" stroke="#2F5FAC" strokeWidth="3" />
           xmlns="http://www.w3.org/2000/svg"
           width="11"
           height="11"
@@ -519,14 +282,7 @@ function MyCourse() {
       <div className="flex flex-col items-center justify-center ">
         <h2 className="H2">My Course</h2>
         <div className="justify-start items-start gap-4 inline-flex mt-[60px]">
-      <div className="flex flex-col items-center justify-center ">
-        <h2 className="H2">My Course</h2>
-        <div className="justify-start items-start gap-4 inline-flex mt-[60px]">
           <div
-            onClick={() => handleCourseFilter('all')}
-            className={`box-content cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
-          >
-            <div className="Body2">All Course</div>
             onClick={() => handleCourseFilter('all')}
             className={`box-content cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
           >
@@ -537,13 +293,9 @@ function MyCourse() {
             className={`box-content cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
           >
             <div className="Body2">Inprogress</div>
-            onClick={() => handleCourseFilter('inProgress')}
-            className={`box-content cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
-          >
-            <div className="Body2">Inprogress</div>
           </div>
           <div
-            onClick={() => handleComplete('All')}
+            onClick={() => handleCourseFilter('complete')}
             className={`box-content  cursor-pointer Component1 p-2 flex items-start gap-2 border-solid border-white border-b-2 hover:border-b-2 hover:border-solid hover:border-black border-t-0 border-r-0 border-l-0  m-0`}
           >
             <div className="Body2">Complete</div>
@@ -555,24 +307,12 @@ function MyCourse() {
           <div className="flex flex-col justify-center items-center">
             <Avatar alt="" src={avatar} sx={{ width: 120, height: 120 }} />
             <h2 className="my-[24px]">{userName}</h2>
-      <div className="flex flex-row mt-[80px] ">
-        <div className="flex flex-col w-[357px] h-[500px] Shadow2 px-[24px] py-[32px] content-center items-center mr-[24px] rounded-lg  sticky top-0 ">
-          <div className="flex flex-col justify-center items-center">
-            <Avatar alt="" src={avatar} sx={{ width: 120, height: 120 }} />
-            <h2 className="my-[24px]">{userName}</h2>
           </div>
           <div className="flex flex-row ">
             <div className="flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]">
               <p className="Body2">Course Inprogress</p>
               <p className="H3">{inProgressCount}</p>
-          <div className="flex flex-row ">
-            <div className="flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]">
-              <p className="Body2">Course Inprogress</p>
-              <p className="H3">{inProgressCount}</p>
             </div>
-            <div className="flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]">
-              <p className="Body2">Course Complete</p>
-              <p className="H3">{completeCount}</p>
             <div className="flex flex-col justify-between p-[16px] w-[142.5px] h-[134px] bg-[--gray200] mx-[12px]">
               <p className="Body2">Course Complete</p>
               <p className="H3">{completeCount}</p>
@@ -584,7 +324,6 @@ function MyCourse() {
                 count={totalPagesAllCourse}
                 onChange={(event, newPage) =>
                   handlePageChange(event, newPage, 'all')
-                  handlePageChange(event, newPage, 'all')
                 }
                 page={currentPage}
                 size="large"
@@ -595,7 +334,6 @@ function MyCourse() {
                 count={totalPagesInprogress}
                 onChange={(event, newPage) =>
                   handlePageChange(event, newPage, 'inProgress')
-                  handlePageChange(event, newPage, 'inProgress')
                 }
                 page={currentPage}
                 size="large"
@@ -605,7 +343,6 @@ function MyCourse() {
               <Pagination
                 count={totalPagesComplete}
                 onChange={(event, newPage) =>
-                  handlePageChange(event, newPage, 'complete')
                   handlePageChange(event, newPage, 'complete')
                 }
                 page={currentPage}
@@ -623,8 +360,6 @@ function MyCourse() {
       </div>
     </div>
   )
-  )
 }
 
-export default MyCourse
 export default MyCourse
