47f920d7f840e11b93a75aa85d86b6d19d118713
piyabud <T_piyabud@outlook.com>
2023-09-26 01:54:19 +0700

    fix : conflict addcoursebyadmin.js file

--- server/apps/addcoursebyadmin.js
+++ server/apps/addcoursebyadmin.js
@@ -9,7 +9,6 @@ addCourseRouter.post('/addcourse', async (req, res) => {
   const dataCourse = {
     course_name: req.body?.course_name,
     price: req.body.price,
-<<<<<<< HEAD
     total_time: req.body.total_time,
     course_summary: req.body.course_summary,
     course_detail: req.body.course_detail,
@@ -35,44 +34,6 @@ addCourseRouter.post('/addcourse', async (req, res) => {
         error_message: error.message,
       })
     }
-=======
-    total_time: req.body?.total_time,
-    course_summary: req.body?.course_summary,
-    course_detail: req.body?.course_detail,
-  };
-
-  console.log(dataCourse);
-  return res.json({ message: dataCourse });
-  // try {
-  //   const { data, error } = await supabase
-  //     .from("courses")
-  //     .insert([dataCourse])
-  //     .select();
-
-  //   // console.log(data);
-
-  //   if (error) {
-  //     return res.status(400).json({
-  //       error: "Upload Course failed",
-  //       error_message: error.message,
-  //     });
-  //   }
-
-  //   // console.log();
-
-  //   res.status(201).json({
-  //     success: "Course uploaded successfully",
-  //     data: data,
-  //   });
-  // } catch (error) {
-  //   console.error("Error:", error);
-  //   res.status(500).json({
-  //     error: "Internal Server Error",
-  //     error_message: error.message,
-  //   });
-  // }
-});
->>>>>>> 17f8f51 (feat : upload img)
 
 
     console.log()
