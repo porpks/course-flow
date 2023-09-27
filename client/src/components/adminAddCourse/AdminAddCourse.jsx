6b994b45bc5eb32c43c72c20f36ffce3d208ff5f
piyabud <T_piyabud@outlook.com>
2023-09-27 14:11:08 +0700

    feat : add SnackBar alert msg when upload media

--- client/src/components/adminAddCourse/AdminAddCourse.jsx
+++ client/src/components/adminAddCourse/AdminAddCourse.jsx
@@ -96,207 +96,141 @@ function AdminAddCourse() {
 
   return (
     <>
-      <div className="canvas flex flex-row">
-        {/* LEFT-NAV */}
-        <div className="">
-          <Sidebar />
-        </div>
-        {/* RIGHT-NAV */}
-        <div className="w-full">
-          <div className="topNav  flex  items-center gap-[16px] px-[40px] py-[16px] w-100% bg  ">
-            <div className="H3 flex-1">Add Course</div>
-            <button className="Secondary Shadow1">Cancel</button>
-            <button className="Primary Shadow1 border-none" onClick={sendData}>
-              Create
-            </button>
+      <div className="flex justify-center items-center">
+        <div className="canvas flex flex-row w-[1440px]">
+          {/* LEFT-NAV */}
+          <div className="">
+            <Sidebar isCoursePage={true} />
           </div>
-          {/* MIDDLE-AREA */}
-          <div className="p-[40px] bg-[--gray100] ">
-            {/* PACKAGE WRAPPER  */}
-            <div className="packageWrapper px-[100px] pt-[40px] pb-[60px] w-full h-full mb-[24px] ">
-              <Formik>
-                <Form
-                  className="flex flex-col  gap-[40px]"
-                  onSubmit={formik.handleSubmit}
-                >
-                  <div className="flex flex-col gap-[4px]">
-                    <label htmlFor="courseName" className="">
-                      Course name *
-                    </label>
-                    <input
-                      type="text"
-                      name="courseName"
-                      // id="courseName"
-                      placeholder="Enter Course Name"
-                      className="Input"
-                      value={formik.values.courseName}
-                      onChange={formik.handleChange}
-                    />
-                    {formik.errors.courseName ? (
-                      <div>{formik.errors.courseName}</div>
-                    ) : null}
-                  </div>
-                  <div className="flex gap-[80px] ">
-                    <div className="flex flex-col flex-1 gap-[4px] ">
-                      <label htmlFor="Price" className="">
-                        Price *
-                      </label>
-                      <input
-                        type="number"
-                        name="price"
-                        // id="price"
-                        placeholder="Enter Course Price"
-                        className="Input"
-                        value={formik.values.price}
-                        onChange={formik.handleChange}
-                      />
-                      {formik.errors.price ? (
-                        <div>{formik.errors.price}</div>
-                      ) : null}
-                    </div>
-                    <div className="flex flex-col flex-1 gap-[4px]">
-                      <label className="">Total learning time *</label>
-                      <input
-                        type="number"
-                        name="totalLearningTime"
-                        // id="totalLearningTime"
-                        placeholder="Enter Total learning time"
-                        className="Input"
-                        value={formik.values.totalLearningTime}
-                        onChange={formik.handleChange}
-                      />{' '}
-                      {formik.errors.totalLearningTime ? (
-                        <div>{formik.errors.totalLearningTime}</div>
-                      ) : null}
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-[4px]">
-                    <label className="">Course summary *</label>
-                    <textarea
-                      type="text"
-                      name="courseSummary"
-                      // id="courseSummary"
-                      placeholder="Enter Course summary"
-                      className="Input h-[100px] resize-none"
-                      value={formik.values.courseSummary}
-                      onChange={formik.handleChange}
-                    />
-                    {formik.errors.courseSummary ? (
-                      <div>{formik.errors.courseSummary}</div>
-                    ) : null}
-                  </div>
-                  <div className="flex flex-col gap-[4px]">
-                    <label className="">Course detail *</label>
-                    <textarea
-                      type="text"
-                      name="courseDetail"
-                      // id="courseDetail"
-                      placeholder="Enter Course detail"
-                      className="Input h-[220px] resize-none flex text- align-text-top "
-                      value={formik.values.courseDetail}
-                      onChange={formik.handleChange}
-                    />
-                    {formik.errors.courseDetail ? (
-                      <div>{formik.errors.courseDetail}</div>
-                    ) : null}
-                  </div>
-                  <div className="flex flex-col gap-[6px]">
-                    <label className="">Cover image *</label>
-                    <div className="relative h-fit">
-                      <div className="relative bg-[--gray100] w-[358px] h-[358px] object-cover	rounded-2xl group	">
-                        <div
-                          className="
-                          upload-position   w-[200px] h-[200px] 
-                          flex flex-col justify-center items-center  
-                          text-[--blue500] text-center text-xl rounded-2xl
-                       "
-                        >
-                          <div className="text-[48px] font-extralight mb-3">
-                            +
-                          </div>
-                          <div className="text-[20px] font-medium">
-                            Upload Image
-                          </div>
-                        </div>
-                        <div
-                          className="
-                          upload-position w-[90%] h-[90%]
-                         hover:bg-[rgba(264,264,264,0.25)] rounded-lg cursor-pointer group-hover:block border-[--blue500] border-[2px] hover:border-dashed"
-                        >
-                          <label
-                            htmlFor="upload"
-                            className="hidden group-hover:block hover:transition  w-full h-full  text-[--blue500] text-center text-xl  cursor-pointer"
-                          >
-                            <input
-                              id="upload"
-                              name="avatar"
-                              type="file"
-                              onChange={(e) => handleFileChange(e)}
-                              hidden
-                            />
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-[6px]">
-                    <label className="">Video Trailer *</label>
-                    <div className="relative h-fit">
-                      <div className="relative bg-[--gray100] w-[358px] h-[358px] object-cover	rounded-2xl group	">
-                        <div
-                          className="
-                          upload-position   w-[200px] h-[200px] 
-                          flex flex-col justify-center items-center  
-                          text-[--blue500] text-center text-xl rounded-2xl
-                       "
-                        >
-                          <div className="text-[48px] font-extralight mb-3">
-                            +
-                          </div>
-                          <div className="text-[20px] font-medium">
-                            Upload Video Trailer
-                          </div>
-                        </div>
-                        <div
-                          className="
-                          upload-position w-[90%] h-[90%]
-                         hover:bg-[rgba(264,264,264,0.25)] rounded-lg cursor-pointer group-hover:block border-[--blue500] border-[2px] hover:border-dashed"
-                        >
-                          <label
-                            htmlFor="upload"
-                            className="hidden hover:block hover:transition w-full h-full  text-[--blue500] text-center text-xl  cursor-pointer"
-                          >
-                            <input
-                              id="upload"
-                              name="avatar"
-                              type="file"
-                              onChange={(e) => handleFileChange(e)}
-                              hidden
-                            />
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button type="submit">Submit</button>
-                </Form>
-              </Formik>
-            </div>
-            {/* add;-lesson */}
-            <div className="add-lesson flex flex-row gap-[16px] items-center mb-[43px]">
-              <div className="H3 flex-1">Lesson</div>
+          {/* RIGHT-NAV */}
+          <div className="w-full">
+            <div className="topNav  flex  items-center gap-[16px] px-[40px] py-[16px] w-100% bg  ">
+              <div className="H3 flex-1">Add Course</div>
+              <button className="Secondary Shadow1">Cancel</button>
               <button
-                className="Primary Shadow1 px-[32px] py-[18px] justify-center border-none"
-                onClick={() => {
-                  handleData()
-                }}
+                className="Primary Shadow1 border-none"
+                onClick={sendData}
               >
-                + Add Lesson
+                Create
               </button>
             </div>
-            {/* table-SubLesson */}
-            <div className="table-SubLesson">
-              <LessonTable />
+            {/* MIDDLE-AREA */}
+            <div className="p-[40px] bg-[--gray100] ">
+              {/* PACKAGE WRAPPER  */}
+              <div className="packageWrapper px-[100px] pt-[40px] pb-[60px] w-full h-full mb-[24px] ">
+                <Formik>
+                  <Form
+                    className="flex flex-col  gap-[40px]"
+                    onSubmit={formik.handleSubmit}
+                  >
+                    <div className="flex flex-col gap-[4px] border-2 border-sky-500">
+                      <label htmlFor="courseName" className="">
+                        Course name *
+                      </label>
+                      <input
+                        type="text"
+                        name="courseName"
+                        // id="courseName"
+                        placeholder="Enter Course Name"
+                        className="Input"
+                        value={formik.values.courseName}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.courseName ? (
+                        <div>{formik.errors.courseName}</div>
+                      ) : null}
+                    </div>
+                    <div className="flex gap-[80px] ">
+                      <div className="flex flex-col flex-1 gap-[4px] ">
+                        <label htmlFor="Price" className="">
+                          Price *
+                        </label>
+                        <input
+                          type="number"
+                          name="price"
+                          // id="price"
+                          placeholder="Enter Course Price"
+                          className="Input"
+                          value={formik.values.price}
+                          onChange={formik.handleChange}
+                        />
+                        {formik.errors.price ? (
+                          <div>{formik.errors.price}</div>
+                        ) : null}
+                      </div>
+                      <div className="flex flex-col flex-1 gap-[4px]">
+                        <label className="">Total learning time *</label>
+                        <input
+                          type="number"
+                          name="totalLearningTime"
+                          // id="totalLearningTime"
+                          placeholder="Enter Total learning time"
+                          className="Input"
+                          value={formik.values.totalLearningTime}
+                          onChange={formik.handleChange}
+                        />{" "}
+                        {formik.errors.totalLearningTime ? (
+                          <div>{formik.errors.totalLearningTime}</div>
+                        ) : null}
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                      <label className="">Course summary *</label>
+                      <Field
+                        as="textarea"
+                        // type="text"
+                        name="courseSummary"
+                        // id="courseSummary"
+                        placeholder="Enter Course summary"
+                        className="Input h-[100px] resize-none"
+                        value={formik.values.courseSummary}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.courseSummary ? (
+                        <div>{formik.errors.courseSummary}</div>
+                      ) : null}
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                      <label className="">Course detail *</label>
+                      <Field
+                        as="textarea"
+                        // type="text"
+                        name="courseDetail"
+                        // id="courseDetail"
+                        placeholder="Enter Course detail"
+                        className="Input h-[220px] resize-none flex text- align-text-top "
+                        value={formik.values.courseDetail}
+                        onChange={formik.handleChange}
+                      />
+                      {formik.errors.courseDetail ? (
+                        <div>{formik.errors.courseDetail}</div>
+                      ) : null}
+                    </div>
+
+                    {/*----------------------- UPLOAD IMG --------------------- */}
+                    <UploadImage />
+                    {/*----------------------- UPLOAD VIDEO --------------------- */}
+                    <UploadVideo />
+                    <button type="submit">Submit</button>
+                  </Form>
+                </Formik>
+              </div>
+              {/* add;-lesson */}
+              <div className="add-lesson flex flex-row gap-[16px] items-center mb-[43px]">
+                <div className="H3 flex-1">Lesson</div>
+                <button
+                  className="Primary Shadow1 px-[32px] py-[18px] justify-center border-none"
+                  onClick={() => {
+                    handleData();
+                  }}
+                >
+                  + Add Lesson
+                </button>
+              </div>
+              {/* table-SubLesson */}
+              <div className="table-SubLesson">
+                <LessonTable />
+              </div>
             </div>
           </div>
         </div>
