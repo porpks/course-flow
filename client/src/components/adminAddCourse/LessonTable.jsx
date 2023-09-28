4604e3c3df846819992b7a73554ba3fc40689a83
Punt Boworn.T <boworn.tr@gmail.com>
2023-09-28 08:46:22 +0700

    Rebase

--- client/src/components/adminAddCourse/LessonTable.jsx
+++ client/src/components/adminAddCourse/LessonTable.jsx
@@ -1,30 +1,31 @@
-import React, { useState, useEffect } from "react";
-import "./lessonTable.css";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import './lessonTable.css'
+import { Link } from 'react-router-dom'
 
 function LessonTable() {
-  const [lessonData, setLessonData] = useState([]);
+  const [lessonData, setLessonData] = useState([])
+
   useEffect(() => {
-    const storage = localStorage.getItem("lesson_data");
+    const lessonDataStorage = localStorage.getItem('lesson_data')
     try {
-      if (storage) {
-        console.log("have storage");
-        const parsedData = JSON.parse(storage);
+      if (lessonDataStorage) {
+        console.log('have storage')
+        const parsedData = JSON.parse(lessonDataStorage)
         // console.log(parsedData)
-        const lessonName = parsedData.lessonName;
+        const lessonName = parsedData.lessonName
         const transformedData = parsedData.map((lesson, index) => ({
           lessonName: lesson.lessonName,
           subLessonData: lesson.subLessonList,
-        }));
-        const subLessonData = { ...parsedData };
-        console.log(subLessonData);
+        }))
+        const subLessonData = { ...parsedData }
+        // console.log(subLessonData)
 
-        setLessonData(transformedData);
+        setLessonData(transformedData)
       }
     } catch (error) {
-      console.error("Error:", error);
+      console.error('Error:', error)
     }
-  }, []);
+  }, [])
 
   // const handleDelete = (id, lessonName) => {
   //   alert(`Deleted lesson with ID ${id} ${lessonName}`)
@@ -33,36 +34,36 @@ function LessonTable() {
   const handleDelete = (index, lessonName) => {
     if (window.confirm(`คุณต้องการลบบทเรียน "${lessonName}" ใช่หรือไม่?`)) {
       try {
-        const storage = localStorage.getItem("lesson_data");
-        if (storage) {
-          const parsedData = JSON.parse(storage);
+        const lessonDataStorage = localStorage.getItem('lesson_data')
+        if (lessonDataStorage) {
+          const parsedData = JSON.parse(lessonDataStorage)
 
           if (index >= 0 && index < parsedData.length) {
             // ลบบทเรียนจากอาร์เรย์ด้วย index
-            parsedData.splice(index, 1);
+            parsedData.splice(index, 1)
 
             // อัปเดตข้อมูลใน Local Storage และ state
-            localStorage.setItem("lesson_data", JSON.stringify(parsedData));
-            setLessonData(parsedData);
+            localStorage.setItem('lesson_data', JSON.stringify(parsedData))
+            setLessonData(parsedData)
 
-            alert(`ลบบทเรียน "${lessonName}" เรียบร้อยแล้ว`);
+            alert(`ลบบทเรียน "${lessonName}" เรียบร้อยแล้ว`)
           } else {
-            alert(`Index ${index} ไม่ถูกต้อง`);
+            alert(`Index ${index} ไม่ถูกต้อง`)
           }
         }
       } catch (error) {
-        console.error("Error:", error);
+        console.error('Error:', error)
       }
     }
-  };
+  }
 
   const handleEdit = (id, lessonName) => {
-    alert(`Edited lesson with ID ${id} ${lessonName}`);
-  };
+    alert(`Edited lesson with ID ${id} ${lessonName}`)
+  }
 
   const countSubLessons = (subLessonList) => {
-    return subLessonList ? subLessonList.length : 0;
-  };
+    return subLessonList ? subLessonList.length : 0
+  }
 
   return (
     <>
@@ -97,7 +98,7 @@ function LessonTable() {
         </tbody>
       </table>
     </>
-  );
+  )
 }
 
-export default LessonTable;
+export default LessonTable
--- server/apps/addcoursebyadmin.js
+++ server/apps/addcoursebyadmin.js
@@ -1,17 +1,17 @@
-import { Router } from "express";
-import supabase from "../utils/db.js";
-import multer from "multer";
-import { v4 as uuidv4 } from "uuid";
+import { Router } from 'express'
+import supabase from '../utils/db.js'
+import multer from 'multer'
+import { v4 as uuidv4 } from 'uuid'
 
-const addCourseRouter = Router();
-const multerUpload = multer({});
+const addCourseRouter = Router()
+const multerUpload = multer({})
 const imageCoverUpload = multerUpload.fields([
-  { name: "cover_img", maxCount: 1 },
-  { name: "video_trailer", maxCount: 1 },
-]);
+  { name: 'cover_img', maxCount: 1 },
+  { name: 'video_trailer', maxCount: 1 },
+])
 
-addCourseRouter.post("/addcourse", imageCoverUpload, async (req, res) => {
-  const courseName = req.body.course_name;
+addCourseRouter.post('/addcourse', imageCoverUpload, async (req, res) => {
+  const courseName = req.body.course_name
   const dataCourse = {
     course_name: req.body?.course_name,
     price: req.body.price,
@@ -20,8 +20,8 @@ addCourseRouter.post("/addcourse", imageCoverUpload, async (req, res) => {
     course_detail: req.body.course_detail,
     cover_img: req.body.cover_img,
     video_trailer: req.body.video_trailer,
-  };
-  console.log(req.files);
+  }
+  console.log(req.files)
   // UPLOAD FILE IMG
   try {
     if (
@@ -29,104 +29,104 @@ addCourseRouter.post("/addcourse", imageCoverUpload, async (req, res) => {
       req.files.cover_img ||
       req.files.video_trailer
     ) {
-      const fileImg = req.files.cover_img[0];
-      const fileImage = new Blob([fileImg.buffer], { type: fileImg.mimetype });
-      const fileVdo = req.files.video_trailer[0];
-      const file_Video = new Blob([fileVdo.buffer], { type: fileVdo.mimetype });
+      const fileImg = req.files.cover_img[0]
+      const fileImage = new Blob([fileImg.buffer], { type: fileImg.mimetype })
+      const fileVdo = req.files.video_trailer[0]
+      const file_Video = new Blob([fileVdo.buffer], { type: fileVdo.mimetype })
       // const fileName = file.originalname.replace(/ /g, "_");
 
       // const { data: dd, error: ee } = await supabase.storage.from('test-avatar').remove(`profile/${userId}`)
 
       const { data: objects, error: err } = await supabase.storage
-        .from("test-avatar")
-        .list(`imgCover/${courseName}`);
+        .from('test-avatar')
+        .list(`imgCover/${courseName}`)
 
       if (err) {
-        console.error("Error listing objects:", err.message);
+        console.error('Error listing objects:', err.message)
       }
 
       // Delete each object in the folder
       for (const object of objects) {
         const { error: errorRemove } = await supabase.storage
-          .from("test-avatar")
-          .remove(`imgCover/${courseName}/${[object.name]}`);
+          .from('test-avatar')
+          .remove(`imgCover/${courseName}/${[object.name]}`)
         if (errorRemove) {
-          console.error("Error remove objects:", errorRemove.message);
+          console.error('Error remove objects:', errorRemove.message)
         }
       }
 
       const { data, error } = await supabase.storage
-        .from("test-avatar")
-        .upload(`imgCover/${courseName}/${uuidv4()}`, fileImage);
+        .from('test-avatar')
+        .upload(`imgCover/${courseName}/${uuidv4()}`, fileImage)
 
       if (error) {
-        console.error(error);
+        console.error(error)
       } else {
-        console.log("File uploaded successfully:", data);
+        console.log('File uploaded successfully:', data)
       }
 
-      const path = data.path;
-      const imgUrl = `${process.env.VITE_SUPABASE_URL}/storage/v1/object/public/test-avatar/${path}`;
+      const path = data.path
+      const imgUrl = `${process.env.VITE_SUPABASE_URL}/storage/v1/object/public/test-avatar/${path}`
 
       try {
         if (req.files.length > 0 || req.files.video_trailer) {
-          const fileVdo = req.files.video_trailer[0];
+          const fileVdo = req.files.video_trailer[0]
           const file_Video = new Blob([fileVdo.buffer], {
             type: fileVdo.mimetype,
-          });
+          })
           // const fileName = file.originalname.replace(/ /g, "_");
 
           // const { data: dd, error: ee } = await supabase.storage.from('test-avatar').remove(`profile/${userId}`)
 
           const { data: objects, error: err } = await supabase.storage
-            .from("test-avatar")
-            .list(`videoTrailer/${courseName}`);
+            .from('test-avatar')
+            .list(`videoTrailer/${courseName}`)
 
           if (err) {
-            console.error("Error listing objects:", err.message);
+            console.error('Error listing objects:', err.message)
           }
 
           // Delete each object in the folder
           for (const object of objects) {
             const { error: errorRemove } = await supabase.storage
-              .from("test-avatar")
-              .remove(`videoTrailer/${courseName}/${[object.name]}`);
+              .from('test-avatar')
+              .remove(`videoTrailer/${courseName}/${[object.name]}`)
             if (errorRemove) {
-              console.error("Error remove objects:", errorRemove.message);
+              console.error('Error remove objects:', errorRemove.message)
             }
           }
 
           const { data, error } = await supabase.storage
-            .from("test-avatar")
-            .upload(`videoTrailer/${courseName}/${uuidv4()}`, file_Video);
+            .from('test-avatar')
+            .upload(`videoTrailer/${courseName}/${uuidv4()}`, file_Video)
 
           if (error) {
-            console.error(error);
+            console.error(error)
           } else {
-            console.log("File uploaded successfully:", data);
+            console.log('File uploaded successfully:', data)
           }
 
-          const path = data.path;
-          const vdoUrl = `${process.env.VITE_SUPABASE_URL}/storage/v1/object/public/test-avatar/${path}`;
+          const path = data.path
+          const vdoUrl = `${process.env.VITE_SUPABASE_URL}/storage/v1/object/public/test-avatar/${path}`
 
           try {
             const { data, error } = await supabase
-              .from("courses")
+              .from('courses')
               .insert({
                 ...dataCourse,
                 cover_img: imgUrl,
                 video_trailer: vdoUrl,
               })
-              .select();
+              .select()
             if (error) {
-              console.log(error);
+              console.log(error)
             }
           } catch (error) {
-            console.error(error);
+            console.error(error)
           }
         }
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
       // try {
       //   const { data, error } = await supabase
@@ -159,8 +159,8 @@ addCourseRouter.post("/addcourse", imageCoverUpload, async (req, res) => {
       // }
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-});
+})
 
-export default addCourseRouter;
+export default addCourseRouter
