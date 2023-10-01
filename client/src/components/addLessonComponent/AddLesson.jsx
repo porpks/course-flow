9fd4a31fe613a74fbae2d9c45543aef3ee08368d
PoR <por.pkp1997@gmail.com>
2023-10-02 00:50:42 +0700

    test

--- client/src/components/addLessonComponent/AddLesson.jsx
+++ client/src/components/addLessonComponent/AddLesson.jsx
@@ -158,6 +158,7 @@ function AddLesson(sharedState, updateState) {
       newLesson.push({ lessonName, subLessonData, subLessonVideo });
       setLesson(newLesson);
     }
+    console.log(lesson);
 
     navigate(`/admin/addcourse`);
     // console.log(data)
@@ -358,8 +359,8 @@ function AddLesson(sharedState, updateState) {
                                     width="100%"
                                     height="100%"
                                     controls={true}
-                                    // light={true}
-                                    // playIcon={"../public/image/playIcon.svg"}
+                                  // light={true}
+                                  // playIcon={"../public/image/playIcon.svg"}
                                   />
                                   {subLesson.subLessonVideo ? (
                                     <button
